440d055f003d4e1ee86eab2a5f57eb3f3dd0883d
agievaliliyaa <agievaliliyaa@gmail.com>
2022-12-02 23:33:29 +0300

    Add project files.

---
+++ HomeWork/Program.cs
@@ -0,0 +1,27 @@
+﻿using HomeWork_03._12._22;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Reflection;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Задача 1");
+
+            Console.WriteLine("Задача 2");
+            Person person1 = new Person("Илья", "программирование");
+            Person person2 = new Person("Аделя", "музыка");
+            Person person3 = new Person("Полина", "сериалы");
+
+        }
+    }
+}
